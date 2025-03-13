e927a5677c538216041d8caebb611ca5ec50a10e
Diren Boran Sezen <boransezen1@gmail.com>
2025-03-13 16:27:56 +0300

    added service methods & exception

--- src/main/java/com/todo/todo_app/service/TaskService.java
+++ src/main/java/com/todo/todo_app/service/TaskService.java
@@ -16,4 +16,34 @@ public class TaskService {
         this.userService = userService;
         this.taskDtoConverter = taskDtoConverter;
     }
+
+
+    // get all tasks of a user by userid
+
+
+    // get all undone tasks of a user by userid
+
+
+    // get all done tasks of a user by userid
+
+
+    // create a task for a user by userid
+
+
+    // get a user's task by userid and taskid
+
+
+    //update a user's task by userid and taskid
+
+
+    //update a user's task as done if it is undone by userid and taskid
+
+
+    //update a user's task as undone if it is done by userid and taskid
+
+
+    //delete a user's task by userid and taskid
+
+
+
 }
--- src/main/java/com/todo/todo_app/service/UserService.java
+++ src/main/java/com/todo/todo_app/service/UserService.java
@@ -1,20 +1,32 @@
 package com.todo.todo_app.service;
 
+import com.todo.todo_app.dto.UserDto;
 import com.todo.todo_app.dto.converter.UserDtoConverter;
+import com.todo.todo_app.model.User;
 import com.todo.todo_app.repository.UserRepository;
+import com.todo.todo_app.exception.UserNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
-    private final TaskService taskService;
     private final UserDtoConverter userDtoConverter;
 
 
     public UserService(UserRepository userRepository, TaskService taskService, UserDtoConverter userDtoConverter) {
         this.userRepository = userRepository;
-        this.taskService = taskService;
         this.userDtoConverter = userDtoConverter;
     }
+
+    // find a user by id. finding a user means finding a user from the repo.
+    public User findUserById(String userId) {
+        return userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User Could Not Find By Id: " + userId));
+    }
+
+    // get a user by id. getting a user means getting a userdto
+    public UserDto getUserById(String userId) {
+        return userDtoConverter.convert(findUserById(userId));
+    }
+
 }
