08612b4087548106bbf93fa9cb9891ffbf2378b9
Diren Boran Sezen <boransezen1@gmail.com>
2025-03-16 17:29:05 +0300

    created UserController

--- src/main/java/com/todo/todo_app/controller/UserController.java
+++ src/main/java/com/todo/todo_app/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.todo.todo_app.controller;
 
+import com.todo.todo_app.dto.TaskDto;
+import com.todo.todo_app.dto.UserCreationRequest;
 import com.todo.todo_app.dto.UserDto;
 import com.todo.todo_app.model.User;
 import com.todo.todo_app.service.UserService;
@@ -8,6 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -18,9 +24,25 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/{userId")
+    @GetMapping("/{userId}")
     public ResponseEntity<UserDto> getUser(@Valid @PathVariable String userId) {
         return ResponseEntity.ok(userService.getUserById(userId));
     }
+
+    @GetMapping("/undone/{userId}")
+    public ResponseEntity<List<TaskDto>> getUndoneTasks(@Valid @PathVariable String userId) {
+        return ResponseEntity.ok(userService.getUndoneTasks(userId));
+    }
+
+    @GetMapping("/done/{userId}")
+    public ResponseEntity<List<TaskDto>> getDoneTasks(@Valid @PathVariable String userId) {
+        return ResponseEntity.ok(userService.getDoneTasks(userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserCreationRequest userCreationRequest) {
+        return ResponseEntity.ok(userService.createUser(userCreationRequest));
+    }
+
 
 }
